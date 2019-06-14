f4f2e5f368b56bd31daa9d86ead3786c0aad3c4f
Damola <damolasd@gmail.com>
2019-06-14 09:45:42 +0100

    Step 2 done: Added Links and Routes

--- village/src/App.js
+++ village/src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import './App.css';
+import styled from 'styled-components';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 
+const NavBar = styled.nav`
+  height: 80px; 
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  background-color: #62CDFD;
+
+  a {
+    border: 1px solid black;
+    padding: 5px;
+    border-radius: 5px;
+    background-color: yellow;
+    margin: 20px;
+    text-decoration: none;
+    cursor: pointer;
+    
+
+    &:hover {
+
+    }
+
+  }
+
+  
+  
+`;
+
 export const smurfServer = 'http://localhost:3333/smurfs';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -20,7 +51,7 @@ class App extends Component {
   getSmurfs = () => {
     Axios.get(smurfServer)
       .then(res => {
-        this.setState({ smurfs: res.data});
+        this.setState({ smurfs: res.data });
       })
   }
 
@@ -34,10 +65,26 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar>
+            <Link to="/">Home</Link>
+            <Link to="/smurf-form">Add Smurf</Link>
+          </NavBar>
+          {/* <SmurfForm /> */}
+          <Smurfs smurfs={this.state.smurfs} />
+
+          <Route
+            path="/"
+            render={props => (this.componentDidUpdate)}
+          />
+
+          <Route
+            path="/smurf-form"
+            component={SmurfForm}
+          />
+        </div>
+      </Router>
     );
   }
 }
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 // import smurfServer from '../App';
+import styled from 'styled-components';
+
 
 const smurfServer = 'http://localhost:3333/smurfs';
+
+const StyledForm = styled.form`
+  input {
+    padding: 10px;
+    margin: 10px;
+    border-radius: 10px;
+
+  }
+
+  button {
+    padding: 10px;
+    margin: 10px;
+    border-radius: 20px;
+    background-color: goldenrod;
+    cursor: pointer;
+  }
+`;
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -41,7 +60,7 @@ class SmurfForm extends Component {
   render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <StyledForm onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
             placeholder="name"
@@ -61,7 +80,7 @@ class SmurfForm extends Component {
             name="height"
           />
           <button type="submit">Add to the village</button>
-        </form>
+        </StyledForm>
       </div>
     );
   }
